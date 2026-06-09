52dc7f1ff996848a7e3b7ed1781beb74f32a5555
Adithkp03 <adithkp03@gmail.com>
2026-06-09 09:26:01 +0530

    server issues

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,7 +12,8 @@ const port = process.env.PORT || 5000;
 
 // CORS Configuration
 const allowedOrigins = [
-  'http://localhost:5173', // Local development
+  'http://localhost:5173',
+  'https://resumize-front.vercel.app/', // Local development
   process.env.FRONTEND_URL // Production Vercel URL
 ].filter(Boolean) as string[];
 console.log("FRONTEND_URL:", process.env.FRONTEND_URL);
